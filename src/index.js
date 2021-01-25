785785c015711e3b90f94b4abcc19673d9bf9f8e
Morgan Segura <morgansegura@gmail.com>
2021-01-24 23:31:57 -0800

    Toubleshooting netlify build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './assets/styles/index.css'
-import App from './app'
+import App from './App'
 import { reportWebVitals } from './core'
 
 ReactDOM.render(
